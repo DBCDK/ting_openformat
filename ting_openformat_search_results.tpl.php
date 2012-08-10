0bf0530186ca5128b5a5e8e3321b09d58aa10635
pjohans <pjohans@gmail.com>
2012-08-10 08:29:24 +0200

    bibdkWork template

--- ting_openformat_search_results.tpl.php
+++ ting_openformat_search_results.tpl.php
@@ -2,47 +2,11 @@
 $search_result = drupal_static('ting_search_results');
 ?>
 
-
-
 <?php
-/*if( is_object($mani) ){
-  print $mani->type->{'$'};
-}
-elseif( is_array($mani) ){
-  $set = array();
-  foreach($mani as $key=>$festation) {
-    if( !in_array( $festation->type->{'$'}, $set ) ) {
-      print $festation->type->{'$'}.',';
-    }
-    $set[]=$festation->type->{'$'};
-  }
-  }*/
+echo drupal_render($variables['work']);
+echo theme('item_list', array('items' => $variables['manifestations']), NULL, 'ul', array('class' => 'manifestation'));
 ?>
 
-<div></div>
-
-<div class="ting_openformat_toggle" style="border:1px solid #CCCCCC">TOGGLE ME
-<div class="ting_openformat_search_result_more">
-<?php
-  /*if( is_object($mani) ){
-  if( isset($mani->details->contributors) ) {
-    print $mani->details->contributors->{'$'};
-  }
-}
-elseif( is_array($mani) ){
-  $set = array();
-  foreach($mani as $key=>$festation) {
-    if( isset( $festation->details->contributors ) ){
-      if( !in_array( $festation->details->contributors->{'$'}, $set ) ) {
-	print $festation->details->contributors->{'$'};
-      }
-      $set[]=$festation->details->contributors->{'$'};
-    }
-  }
-  }*/
-?>
-</div>
-</div>
 <hr />
 
 
