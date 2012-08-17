683ececa95d45c1b8eea87c6e42adc26883e67a6
pjohans <pjohans@gmail.com>
2012-08-17 12:24:42 +0200

    templates and javascript for showing work

--- js/ting_openformat.js
+++ js/ting_openformat.js
@@ -8,8 +8,17 @@
 (function ($) {
     Drupal.behaviors.ting_openformat_toggle_search = {
         attach: function(context) {
-            $("div.ting_openformat_toggle").click(function() {
-		$(this).find("div.ting_openformat_search_result_more").toggle();
+            $("div.ting_openformat_subwork_tab").click(function() {
+		var id = "ting_openformat_subwork_"+this.id;
+		var children =$('[id="ting_openformat_subwork_'+this.id+'"]').parent('.ting_openformat_subworks').children('.ting_openformat_subwork');
+		$(children).each(function(index){
+		    if( this.id != id ) {
+			$(this).hide();
+		    }
+		    else {
+			$(this).toggle('5');;
+		    }
+		});
 	    });
 	}
     };
--- theme/ting_openformat_subwork_tabs.tpl.php
+++ theme/ting_openformat_subwork_tabs.tpl.php
@@ -1,8 +1,13 @@
+<div class="ting_openformat_subwork_tabs">
 <?php
 foreach( $variables['subWorks'] as $type => $manifestations ) {
   // use first id of manifestations as id for subwork tab
-  echo '<div id="'.key($manifestations).'" class="ting_openformat_subwork_tab">';
+  // $css_id = str_replace(':','-',key($manifestations)); 
+   $css_id = key($manifestations);
+  echo '<div id="'.$css_id.'" class="ting_openformat_subwork_tab">';
   echo $type;
   echo '</div>';
 }
 ?>
+</div>
+<div class="break"></div>
