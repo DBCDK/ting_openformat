28de0080ce0cca36ce6a40e0573c5feba40dc83a
pjohans <pjohans@gmail.com>
2012-08-28 09:20:59 +0200

    changes to handle subworks as entities

--- theme/ting_openformat_subwork_tabs.tpl.php
+++ theme/ting_openformat_subwork_tabs.tpl.php
@@ -1,11 +1,8 @@
 <div class="ting_openformat_subwork_tabs">
 <?php
-foreach( $variables['subWorks'] as $type => $manifestations ) {
-  // use first id of manifestations as id for subwork tab
-  // $css_id = str_replace(':','-',key($manifestations)); 
-   $css_id = key($manifestations);
-  echo '<div id="'.$css_id.'" class="ting_openformat_subwork_tab">';
-  echo $type;
+foreach( $variables['subWorks'] as $id => $subWork ) {
+  echo '<div id="'.$id.'" class="ting_openformat_subwork_tab">';
+  echo $subWork->getType();
   echo '</div>';
 }
 ?>
