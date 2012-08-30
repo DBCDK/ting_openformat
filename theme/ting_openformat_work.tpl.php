4df799f4cb5c0f919336a7ea3c068f18d06cbd6f
Søren Vibjerg <vibjerg@gmail.com>
2012-08-30 14:26:28 +0200

    - Added field render function to Subworks - Changes SubWork bundle to Subwork - Added isReservable function to Manifistation and Subwork - Added getIDs function to Manifisation and Subwork to retrieve multiple IDs as an array

--- theme/ting_openformat_work.tpl.php
+++ theme/ting_openformat_work.tpl.php
@@ -4,11 +4,13 @@
 print drupal_render($variables['fields']);
 print $variables['tabs'];
 echo '<div class="ting_openformat_subworks">';
-foreach( $variables['subWorks'] as $type => $manifestations ) {
+foreach( $variables['subWorks'] as $type => $subworks ) {
+  $manifestations = $subworks['manifistations'];  
   //$css_id = str_replace(':','-',key($manifestations));
   $css_id = key($manifestations);
 
   echo '<div id="ting_openformat_subwork_'.$css_id.'" class="ting_openformat_subwork">';
+  echo drupal_render($subworks['fields']);
   foreach( $manifestations as $id => $manifestation ) {
     echo '<h3>'.$id.'</h3>';
     print $manifestation;
