2cf99d7203dded96a8559af388e42d5a176d9f03
Jorgen G. Nielsen <jgn@dbc.dk>
2013-01-31 12:39:45 +0100

    'localisation_link' renamed to 'subtype_actions'. Added .api.php file

---
+++ ting_openformat.api.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * @file
+ * Hooks provided by the Ting openformat module.
+ */
+
+/**
+ * @addtogroup hooks
+ * @{
+ */
+
+/**
+ * Add actions to subwork material types.
+ *
+ * @param $ordered_subworks
+ * @param $type_id
+ * @param $subtype_id
+ * @return array
+ *  A render array
+ */
+function hook_ting_openformat_subwork_materialtype_actions($ordered_subworks, $type_id, $subtype_id) {
+}
+
+/**
+ * @} End of "addtogroup hooks".
+ */
