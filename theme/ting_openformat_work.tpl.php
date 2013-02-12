5ac3c3a1ef77babb923827347d22c5173cfec8b1
Martin Møller <mmj@dbc.dk>
2013-02-12 09:37:07 +0100

    Formatting

--- theme/ting_openformat_work.tpl.php
+++ theme/ting_openformat_work.tpl.php
@@ -4,15 +4,15 @@
 print drupal_render($variables['fields']);
 print $variables['tabs'];
 echo '<div class="ting_openformat_subworks">';
-foreach( $variables['subWorks'] as $type => $subworks ) {
-  $manifestations = $subworks['manifestations'];  
+foreach ($variables['subWorks'] as $type => $subworks) {
+  $manifestations = $subworks['manifestations'];
   //$css_id = str_replace(':','-',key($manifestations));
   $css_id = key($manifestations);
 
-  echo '<div id="ting_openformat_subwork_'.$css_id.'" class="ting_openformat_subwork">';
+  echo '<div id="ting_openformat_subwork_' . $css_id . '" class="ting_openformat_subwork">';
   echo drupal_render($subworks['fields']);
-  foreach( $manifestations as $id => $manifestation ) {
-    echo '<h3>'.$id.'</h3>';
+  foreach ($manifestations as $id => $manifestation) {
+    echo '<h3>' . $id . '</h3>';
     print $manifestation;
   }
   echo '</div>';
