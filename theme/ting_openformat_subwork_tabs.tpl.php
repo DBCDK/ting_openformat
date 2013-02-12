8dfdb04b6cde9a62c50b5e5a0f28f12dbb0ec792
Martin Møller <mmj@dbc.dk>
2013-02-12 09:35:02 +0100

    Formatting

--- theme/ting_openformat_subwork_tabs.tpl.php
+++ theme/ting_openformat_subwork_tabs.tpl.php
@@ -1,11 +1,11 @@
 <div class="ting_openformat_subwork_tabs">
-<?php 
-$count = 0;
-foreach( $variables['subWorks'] as $type => $subWork ) {
-  echo '<div id="'.$subWork['id'].'" class="ting_openformat_subwork_tab">';
-  echo $subWork['type'];
-  echo '</div>';
-}
-?>
+  <?php
+  $count = 0;
+  foreach ($variables['subWorks'] as $type => $subWork) {
+    echo '<div id="' . $subWork['id'] . '" class="ting_openformat_subwork_tab">';
+    echo $subWork['type'];
+    echo '</div>';
+  }
+  ?>
 </div>
 <div class="break"></div>
