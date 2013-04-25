66d3b373a04ae8018f3d7fe86feb9153a5902c33
Søren Vibjerg <vibjerg@gmail.com>
2013-04-25 11:00:37 +0200

    add template for single work view

---
+++ theme/ting_openformat_single_work.tpl.php
@@ -0,0 +1,7 @@
+<div class="wrapper" data-ids="<?php print $ids; ?>">
+  <h4><?php print $title; ?></h4>
+  <div><?php print $author; ?></div>
+  <div id="">
+    <?php print $work; ?>
+  </div>
+</div>
