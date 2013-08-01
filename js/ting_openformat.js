98d0943386eb3a7f32aae40ebdc731e336d52d62
Søren Vibjerg <vibjerg@gmail.com>
2013-08-01 13:04:29 +0200

    cleanup

--- js/ting_openformat.js
+++ js/ting_openformat.js
@@ -16,48 +16,33 @@
 
     }
 
-    Drupal.behaviors.ting_openformat_toggle_search = {
+    Drupal.behaviors.ting_openformat = {
         attach: function (context) {
 
-            $("div.ting_openformat_subwork_tab").click(function () {
-                var id = "ting_openformat_subwork_" + this.id;
-                var children = $('[id="ting_openformat_subwork_' + this.id + '"]').parent('.ting_openformat_subworks').children('.ting_openformat_subwork');
-                $(children).each(function (index) {
-                    if (this.id != id) {
-                        $(this).hide();
-                    }
-                    else {
-                        $(this).toggle('5');
-                    }
-                });
-            });
-
-            TingOpenformat.getManifestations();
-
-
-            //Control zebra-toggle
-            //Control zebra-toggle
-
-            //Control zebra-toggle
-            /*$('.zebra-toggle a').once().click(function (e){
-             e.preventDefault();
-             var id = $(this).attr('href');
-             $(this).toggleClass('toggled');
-             $(this).children('.toggle-text').toggleClass('hidden');
-             var ids = new Array();
-             $(this).parents(id).find(".manifestation-container").each(function(i){
-
-             ids.push($(this).attr('data-id'));
-             });
-
-             });*/
+            TingOpenformat.activateSubWorkTabs(context);
+            TingOpenformat.loadManifestationsWithAjax(context);
         }
     };
 
-    TingOpenformat.getManifestations = function () {
+    TingOpenformat.activateSubWorkTabs = function (context) {
+        $("div.ting_openformat_subwork_tab", context).click(function () {
+            var id = "ting_openformat_subwork_" + this.id;
+            var children = $('[id="ting_openformat_subwork_' + this.id + '"]').parent('.ting_openformat_subworks').children('.ting_openformat_subwork');
+            $(children).each(function (index) {
+                if (this.id != id) {
+                    $(this).hide();
+                }
+                else {
+                    $(this).toggle('5');
+                }
+            });
+        });
+    }
+
+    TingOpenformat.loadManifestationsWithAjax = function (context) {
 
 
-        $('.tabs-nav-sub a, .zebra-toggle a').once().each(function (e) {
+        $('.tabs-nav-sub a, .zebra-toggle a', context).once().each(function (e) {
             var wrapper_id = $(this).attr('href');
             var manifestation_ids = new Array();
             $(wrapper_id).find(".manifestation-container").each(function (i) {
@@ -65,21 +50,18 @@
                 $(this).html('<div class="ajax-progress ajax-progress-throbber"><div class="throbber">&nbsp;</div></div>');
             });
 
-            // enable ajax
-            TingOpenformat.addAjax($(this), manifestation_ids);
-
+            var url = Drupal.settings.basePath + Drupal.settings.pathPrefix + 'ting_openformat/ajax/manifestations/' + JSON.stringify(manifestation_ids);
+            TingOpenformat.addAjaxToElement($(this), url);
 
         });
 
 
     } // end getManifestations
-    TingOpenformat.addAjax = function (element, manifestation_ids) {
-
-        var base = Drupal.settings.basePath + Drupal.settings.pathPrefix + 'ting_openformat/ajax/manifestations/' + JSON.stringify(manifestation_ids);
+    TingOpenformat.addAjaxToElement = function (element, url, event) {
 
         var element_settings = {};
-        element_settings.url = base;
-        element_settings.event = 'load_manifestations';
+        element_settings.url = url;
+        element_settings.event = (typeof event != 'undefined') ? event : 'load_manifestations';
         element_settings.progress = { type: 'throbber' };
 
         var id = element.attr('id');
