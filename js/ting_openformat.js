4a17de6141e0d8a84b1d090b29f31b4fb2172149
Søren Vibjerg <vibjerg@gmail.com>
2013-08-02 08:02:02 +0200

    more cleanup

--- js/ting_openformat.js
+++ js/ting_openformat.js
@@ -9,13 +9,6 @@
 
     var TingOpenformat = {}
 
-    Drupal.ajax.prototype.commands.add_manifestations = function (ajax, response, status) {
-        console.log(ajax);
-        console.log(response);
-        console.log(status);
-
-    }
-
     Drupal.behaviors.ting_openformat = {
         attach: function (context) {
 
@@ -42,7 +35,7 @@
     TingOpenformat.loadManifestationsWithAjax = function (context) {
 
 
-        $('.tabs-nav-sub a, .zebra-toggle a', context).once().each(function (e) {
+        $('.zebra-toggle a', context).once().click(function (e) {
             var wrapper_id = $(this).attr('href');
             var manifestation_ids = new Array();
             $(wrapper_id).find(".manifestation-container").each(function (i) {
@@ -50,30 +43,43 @@
                 $(this).html('<div class="ajax-progress ajax-progress-throbber"><div class="throbber">&nbsp;</div></div>');
             });
 
-            var url = Drupal.settings.basePath + Drupal.settings.pathPrefix + 'ting_openformat/ajax/manifestations/' + JSON.stringify(manifestation_ids);
-            TingOpenformat.addAjaxToElement($(this), url);
+            TingOpenformat.addAjaxToElement($(this), manifestation_ids);
 
         });
 
 
-    } // end getManifestations
-    TingOpenformat.addAjaxToElement = function (element, url, event) {
+        $('.tabs-nav-sub a', context).once().click(function (e) {
 
+            var wrapper_id = $(this).attr('href');
+            // We load the first manifestation only if it has not been loaded before.
+            if ($(wrapper_id).find('.manifestation').first().find('.manifestation-container').length > 0) {
+                var manifestation_id = $(wrapper_id).find(".manifestation-container").first()
+                    .html('<div class="ajax-progress ajax-progress-throbber"><div class="throbber">&nbsp;</div></div>')
+                    .attr('data-id');
+
+                var manifestation_ids = new Array(manifestation_id);
+
+                TingOpenformat.addAjaxToElement($(this), manifestation_ids);
+            }
+        });
+    }
+
+    TingOpenformat.addAjaxToElement = function (element, manifestation_ids, event) {
+        if (manifestation_ids.length == 0)
+            return false;
+
+        var url = Drupal.settings.basePath + Drupal.settings.pathPrefix + 'ting_openformat/ajax/manifestations/' + JSON.stringify(manifestation_ids);
         var element_settings = {};
         element_settings.url = url;
         element_settings.event = (typeof event != 'undefined') ? event : 'load_manifestations';
         element_settings.progress = { type: 'throbber' };
+        element_settings.submit = { spil: 'fisk' };
 
         var id = element.attr('id');
         Drupal.ajax[id] = new Drupal.ajax(id, element, element_settings);
+        element.trigger('load_manifestations');
+        element.unbind('load_manifestations');
 
-        element.click(function (e) {
-            e.preventDefault();
-            // we make shure the ajax call is only triggered first time
-            $(this).trigger('load_manifestations');
-            $(this).unbind('load_manifestations');
-
-        });
     }
 
 
