4f1909e15ed012d324bf84bab72303d0fb9ab949
Søren Vibjerg <vibjerg@gmail.com>
2014-10-22 13:33:54 +0200

    add template for show more text blocks

---
+++ theme/ting_openformat_toggle_more_content.tpl.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * @file
+ * template for show-more/show-less text fields
+ *
+ * Available variables:
+ *  $trimmed_text: the part of the text that is visible by default
+ *  $more_text: the part of the text that can be toggled
+ */
+?>
+<span class='toggle-more'>
+<?php echo $trimmed_text; ?>
+  <span class='toggle-more-content hide'>
+    <?php echo $more_text; ?>
+  </span>
+  <a class='toggle-link' href='#show-more'>
+    <span class="icon icon-left icon-blue-down">&nbsp;</span>
+    <span class='toggle-text show'><?php echo t('show more', array(), array('context' => 'ting_openformat')); ?></span>
+    <span class='toggle-text hide'><?php echo t('show less', array(), array('context' => 'ting_openformat')); ?></span>
+  </a>
+</span>
