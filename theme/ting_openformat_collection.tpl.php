657b2341db7c08744236486cb7c4b6393b80d390
Søren Vibjerg <vibjerg@gmail.com>
2014-12-04 14:36:07 +0100

    cleanup collection tpl

--- theme/ting_openformat_collection.tpl.php
+++ theme/ting_openformat_collection.tpl.php
@@ -13,46 +13,42 @@
  * $work: If in full view mode it contains a work else an ajax wrapper
  *
  */
-kpr($variables);
 ?>
-
 <div <?php echo drupal_attributes($theme_attributes); ?>>
 
   <div class="element">
-
-    <div class="work-header element-section">
+  <div class="element-section">
+    <div class="work-header clearfix">
 
       <div class="element-title">
         <h2><?php print $title; ?></h2>
 
         <h3><?php print $author; ?></h3>
         <span><?php print $part_of; ?></span>
-
       </div>
 
       <div class="element-types-actions">
         <div class="element-types">
           <?php print $types; ?>
         </div>
-
         <div class="element-actions">
           <?php print drupal_render($actions); ?>
         </div>
       </div>
       <!-- element-types-actions -->
-
-      <div class="toggle-work clearfix">
-        <a href="#work_<?php print $uid; ?>" class="works-control work-toggle-element id="selid-<?php print $uid; ?>">
-          <span class="icon icon-left icon-blue-down">&nbsp;</span>
-          <span class="toggle-text"><?php print t('More info'); ?></span>
-          <span class="toggle-text hidden"><?php print t('Less info'); ?></span>
-        </a>
-      </div>
-    </div><!-- work-header-->
-    <div class="work-body work-body-has-cover element-section">
-      <?php drupal_render($work); ?>
+    </div>
+    <!-- work-header -->
+    <div class="work-toggle" data-work-toggle>
+      <a href="#work_<?php print $uid; ?>" id="selid-<?php print $uid; ?>">
+      <span class="icon icon-left icon-blue-down">&nbsp;</span>
+      <span class="work-toggle-text"><?php print t('More info'); ?></span>
+      <span class="work-toggle-text work-hidden"><?php print t('Less info'); ?></span>
+      </a>
+    </div><!-- work-toggle -->
+  </div><!-- element-section -->
+    <div class="work-body work-body-has-cover element-section work-hidden">
+      <?php print drupal_render($work); ?>
     </div>
     <!-- work-body -->
-
   </div>
 </div>
