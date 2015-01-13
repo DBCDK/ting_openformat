ad0a97e33f0169364aa14e5dcad0e623a82dc7c9
Søren Vibjerg <vibjerg@gmail.com>
2015-01-13 11:25:55 +0100

    separate wrapper for row element

--- theme/ting_openformat_work.tpl.php
+++ theme/ting_openformat_work.tpl.php
@@ -22,18 +22,20 @@
 ?>
 <div class="work-information" id="<?php echo $id; ?>"
      selenium-id="work-<?php echo $id; ?>">
-  <div class="work-fields clearfix row">
-    <div class="show-for-small small-16 columns">
-      <?php print drupal_render($fields['ting_openformat_work_abstract']); ?>
-      <?php print drupal_render($fields['ting_openformat_work_subjects']); ?>
+  <div class="work-fields clearfix">
+    <div class="row">
+      <div class="show-for-small small-16 columns">
+        <?php print drupal_render($fields['ting_openformat_work_abstract']); ?>
+        <?php print drupal_render($fields['ting_openformat_work_subjects']); ?>
+      </div>
+      <div class="work-tabs hide-for-small medium-16 large-18 columns">
+        <?php print drupal_render($fields); ?>
+      </div>
+      <div class="work-cover-wrapper small-8 large-6 columns">
+        <?php print $cover; ?>
+      </div>
     </div>
-    <div class="work-tabs hide-for-small medium-16 large-18 columns">
-      <?php print drupal_render($fields); ?>
-    </div>
-    <div class="work-cover-wrapper small-8 large-6 columns">
-      <?php print $cover; ?>
-    </div>
-  </div>
+  </div><!-- work-information -->
   <div class="work-accordion">
     <dl class="accordion" data-accordion>
       <?php foreach ($subworks as $key => $group) : ?>
@@ -59,5 +61,5 @@
         </dd>
       <?php endforeach; ?>
     </dl>
-  </div>
+  </div><!-- work-accordion -->
 </div>
