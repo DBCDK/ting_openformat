1d4e15dbf4bad082b085f48cc06fd11620698a55
Jørgen G. Nielsen <jgn@dbc.dk>
2018-05-18 15:24:55 +0200

    add recommender css and cover js

--- theme/ting_openformat_work.tpl.php
+++ theme/ting_openformat_work.tpl.php
@@ -67,8 +67,10 @@
     </dl>
   </div><!-- work-accordion -->
   
-  <?php $uids = array(); 
-  foreach ($subworks as $key => $group) { $uids = array_merge($uids, $group['subtype_order_ids']); } ?>
-  <div class="js-recommender" data-uids="<?php echo implode(',', $uids); ?>">
-  </div>
+  <?php 
+  $uids = array(); 
+  foreach ($subworks as $key => $group) { $uids = array_merge($uids, $group['subtype_order_ids']); }
+  ?>
+  <div id="<?php print 'slick-recommender-' . $uid; ?>" 
+       class="js-slick-recommender" data-uids="<?php echo implode(',', $uids); ?>"></div>
 </div>
